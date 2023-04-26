d473eeb11550825fe32183bffb85ef09659b0d3e
ReinaldoJuunior <gamers.jrxiii@gmail.com>
2023-04-26 14:52:08 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react'
+import Header from './Header'
+import Generos from './Generos'
+import NovoGenero from './NovoGenero'
+import EditarGenero from './EditarGenero'
+
+import axios from 'axios'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
+
+
+const Home = () => {
+  return <h1>Home</h1>
+}
 
 function App() {
+ const [data, setData ]  = useState({})
+
+ useEffect( () => {
+    axios.get('/api').then(res => {
+      setData(res.data)
+    })
+ },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header/>
+        <Routes>
+          <Route path='/' exact Component={Home} />
+          <Route path='/generos/:id' exact Component={EditarGenero} />
+          <Route path='/generos/novo' exact Component={NovoGenero} />
+          <Route path='/generos' exact Component={Generos} />
+        </Routes>
+       <pre>{ JSON.stringify(data) }</pre>
+      </div>
+    </Router>
   );
 }
 
